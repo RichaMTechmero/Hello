861cc670ff22b35c21243d354ccdb1ab45905e59
RichaMTechmero <148053261+RichaMTechmero@users.noreply.github.com>
2023-11-09 15:01:30 +0530

    Initial Commit

---
+++ GithubActionProject1122/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubActionProject1122
+//
+//  Created by Techmero on 09/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubActionProject1122UITests/GithubActionProject1122UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GithubActionProject1122UITestsLaunchTests.swift
+//  GithubActionProject1122UITests
+//
+//  Created by Techmero on 09/11/23.
+//
+
+import XCTest
+
+final class GithubActionProject1122UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
